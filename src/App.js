d8ebbb16268b1a42d25432765ba43ee42a6dc15a
Wendell White <88561770+wendellwhitedev@users.noreply.github.com>
2021-09-13 22:34:11 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE'
+ apiKey: '0aebc91376ef43ae83650614e1c84fbd'
 });
 
 const particlesOptions = {
